e2ae09cc27affff46a1b7b98a696ad877d8af61b
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2025-03-03 15:24:45 +0600

    Add quiz schema for managing quiz data

---
+++ Schemas/quizzSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const {array} = require('joi');
+const quizSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    questions:{
+        type:Array,
+        default:[]
+    },
+    createdBy:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
+module.exports = quizSchema;
